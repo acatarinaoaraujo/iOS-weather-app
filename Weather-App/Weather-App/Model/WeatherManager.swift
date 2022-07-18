c79bac308562633626e6f2652460453e0f919b9b
acatarinaoaraujo <acoa_@hotmail.com>
2022-07-17 21:17:40 -1000

    update

--- Weather-App/Weather-App/Model/WeatherManager.swift
+++ Weather-App/Weather-App/Model/WeatherManager.swift
@@ -15,7 +15,7 @@ protocol WeatherManagerDelegate {
 }
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=e72ca729af228beabd5d20e3b7749713&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=c6856320ce2188319591d654c250e2dd&units=metric"
     
     var delegate: WeatherManagerDelegate?
     
